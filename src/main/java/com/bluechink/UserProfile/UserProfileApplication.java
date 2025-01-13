c149136bb532c810589d78e61d1a8381f7584cea
Pritesh2004 <pmpatilpritesh123@gmail.com>
2025-01-13 15:57:48 +0530

    first commit

---
+++ src/main/java/com/bluechink/UserProfile/UserProfileApplication.java
@@ -0,0 +1,13 @@
+package com.bluechink.UserProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileApplication.class, args);
+	}
+
+}
